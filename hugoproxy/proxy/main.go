cfa2fc4439e378ded95a8ba9d4a847f60bc44322
Dima Chanchikov <ara.etot@mail.ru>
2023-12-19 14:23:35 +0700

    testim

--- hugoproxy/proxy/main.go
+++ hugoproxy/proxy/main.go
@@ -1,26 +1,22 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	"net/http"
 	"proxy/internal/controller"
-	"proxy/internal/repository"
+
 	"proxy/internal/service"
-	"proxy/internal/service/models"
+
 	"proxy/router"
 )
 
 //
 
 func main() {
-	us := models.NewUser("OG", 07)
-	db := &sql.DB{}
-	nDb := repository.NewPostgresUserRepository(db)
-	nDb.Create(context.Background(), *us)
+
 	cl := &http.Client{}
 	geoService := service.NewGeoServicer(cl)
 	geoController := controller.NewController(geoService)
+
 	r := router.StartRouter(geoController)
 
 	http.ListenAndServe(":8080", r)
